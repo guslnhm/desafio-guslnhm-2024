e57b2c3127aada482bbcabeb4cb1dea72fb9dd91
Gustavo Linhares <143884487+guslnhm@users.noreply.github.com>
2024-09-11 14:43:34 -0300

    v4

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -5,6 +5,7 @@ class RecintosZoo {
             {
                 id: 1,
                 tamanho_total: 10,
+                espaco_livre: 7,
                 especies:
                 {
                     'MACACO': 3
@@ -18,15 +19,18 @@ class RecintosZoo {
             {
                 id: 2,
                 tamanho_total: 5,
+                espaco_livre: 5,
                 especies:{},
                 permite_max:
                 {
                     'MACACO': 5
                 }
+                /*TEM QUE COLOCAR NO MÍNIMO 2 MACACOS*/
             },
             {
                 id: 3,
                 tamanho_total: 7,
+                espaco_livre: 5,
                 especies:
                 {
                     'GAZELA': 1
@@ -41,6 +45,7 @@ class RecintosZoo {
             {
                 id: 4,
                 tamanho_total: 8,
+                espaco_livre: 8,
                 especies:
                 {},
                 permite_max:
@@ -52,6 +57,7 @@ class RecintosZoo {
             {
                 id: 5,
                 tamanho_total: 9,
+                espaco_livre: 6,
                 especies:
                 {
                     'LEÃO': 1
@@ -66,6 +72,10 @@ class RecintosZoo {
         this.animaisValidos = new Set(["LEAO", "MACACO", "LEOPARDO", "GAZELA", "CROCODILO", "HIPOPOTAMO"]);
 
         this.animal_invalido = {"erro":"Animal inválido"}
+        
+        this.quantidade_invalida = {"erro": "Quantidade inválida"};
+        
+        this.sem_recinto_viavel = {"erro": "Não há recinto viável"};
 
         this.recintosViaveis = [];
         
@@ -73,6 +83,21 @@ class RecintosZoo {
         {
             recintosViaveis: this.recintosViaveis
         };
+        
+        this.x = 0;
+        
+        this.tamanho_animais = {
+            LEAO: 3,
+            LEOPARDO: 2,
+            CROCODILO: 3,
+            MACACO: 1,
+            GAZELA: 2,
+            HIPOPOTAMO: 4
+        }
+        
+        this.espaco_sobra = 0;
+        
+        /*juntar tamanho_animais e animaisValidos, e checar se o animal é válido percorrendo as keys desse object resultante*/
     }
 
     analisaRecintos(animal, quantidade) {
@@ -82,12 +107,17 @@ class RecintosZoo {
         }
         
         for(let recinto of this.recinto){
-            //console.log(recinto.especies);
             if(recinto.permite_max.hasOwnProperty(animal)){
-                //console.log(recinto);
-                this.recintosViaveis.push(`Recinto ${recinto.id} (espaço livre: ${recinto.id} total:${recinto.tamanho_total})`);
+                if(recinto.permite_max[animal] >= quantidade){ //posso adicionar um && com permite_min em cada object
+                    this.x++;
+                    this.espaco_sobra = recinto.espaco_livre - (this.tamanho_animais[animal] * quantidade);
+                    //verifica se tem espécies diferentes, se sim, tira 1 de espaço que sobra
+                    if(!recinto.especies.hasOwnProperty(animal) && Object.keys(recinto.especies)!=0) this.espaco_sobra--;
+                    this.recintosViaveis.push(`Recinto ${recinto.id} (espaço livre: ${this.espaco_sobra} total: ${recinto.tamanho_total})`);
+                }
             }
         }
+        if(this.x==0) return console.log(this.sem_recinto_viavel);
         console.log(this.lista_recintosViaveis);
     }
 
